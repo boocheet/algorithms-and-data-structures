0e48fd8e6fd340aadd6878b42f5d99425b840d42
Tahshara Boochee <boocheet@gmail.com>
2020-06-01 09:22:15 -0700

    completed doubly-linked-list

--- linked-list/doubly-linked-list.js
+++ linked-list/doubly-linked-list.js
@@ -105,12 +105,112 @@
         // return the list
         return this;
      }
+     print(){
+        let arr = []
+        let current = this.head
+        while(current){
+          arr.push(current.val)
+          current = current.next
+        }
+        console.log(arr)
+      }
+
+     get(index){
+        // if index is < 0 or >= the length return null 
+        if (index < 0 || index >= this.length) return null;
+        let half = this.length / 2; 
+        let current = index <= half ? this.head : this.tail;
+        // if index <=  half the length of the list 
+        if (index <= half){
+            // loop through the list starting from the head and loop towards the middle
+            for(let i =0; i < half; i++){
+                if (index === i){
+                    // return the nod once it is found
+                    return current
+                }
+                current = current.next
+            }
+        } else {// if the index is > half the length of the list
+            //loop through the list starting from the tail and loop towards the middle
+            for(let i = this.length -1; i > half; i--){
+                if(index === i) return current;// return node once it's found
+                current = current.prev;
+            }
+        }
+
+     }
+     set(index, val){
+         //create a variable which is the result of the get method
+         //at the index passed to the function
+         let result = this.get(index)
+        //if the get method returns a valid node
+        if(result != null){
+            //set the value of that node to be the value passed to the function 
+            result.val = val; 
+            //return true
+            return true
+        } 
+        //otherwise return false
+        return false;
+     }
+     insert(index, val){
+        //if index is < 0 or >= length return false
+        if(index < 0 || index >= this.length) return false
+        //if index is 0 unshift
+        if(index === 0) return this.unshift(val)
+        //if index is the same as length push 
+        if(index === this.length) return this.push(val)
+        // use get method to access the index -1 
+        let prevNode = this.get(index - 1)
+        // console.log('found node', prevNode)
+        let afterNode = prevNode.next
+        let newNode = new Node(val)
+        // set the next and prev properties on the correct nodes to link 
+        // everything together
+        prevNode.next = newNode
+        newNode.prev = prevNode
+        newNode.next = afterNode
+        afterNode.prev = newNode
+        // increment this.length
+        this.length++
+        //return true
+        return true
+     }
+     remove(index){
+         //if index < 0 or >= length return undefined
+         if(index < 0 || index >= this.length) return undefined
+         //if index is 0, shift
+         if(index === 0) return this.shift();
+         //if index is length -1, pop
+         if(index === this.length -1) return this.pop()
+         // use get method to get the item to be removed
+         let nodeToBeRemoved = this.get(index)
+         let prevNode = nodeToBeRemoved.prev
+         let afterNode = nodeToBeRemoved.next
+         //update the next and prev var to remove found node from list
+         prevNode.next = afterNode
+         afterNode.prev = prevNode
+         //set next and prev to null on the found node
+         nodeToBeRemoved.next = null 
+         nodeToBeRemoved.prev = null 
+         // decrement the length
+         this.length--
+         //return the removed node
+         return this; 
+     }
  }
 
 list = new DoublyLinkedList()
-list.push(99)
-list.push(100)
+list.push('harry')
+list.push('Potter')
+list.push('And')
+list.push('The')
+list.push('James')
 list.push('last item')
 list.shift()
 list.unshift(99)
- console.log(list)
+// console.log(list.set(4, 'test2'))
+list.insert(3, 'Hermione')
+console.log(list.print())
+list.remove(3)
+console.log(list.print())
